ec371024aff1b30853cd2425c536d1b027181017
odysseus5757 <stephen.cowen@gmail.com>
2015-06-03 13:15:03 -0700

    updated

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,5 +2,6 @@ class Classification < ActiveRecord::Base
 
   validates :classification_name, :presence => true, :uniqueness => true
 
+  has_many :messages
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,6 @@ class Message < ActiveRecord::Base
 
   belongs_to :matter
   belongs_to :user
+  belongs_to :classification
 
 end
