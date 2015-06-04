b93a22f93e059b1313b31ba7dbb451587b8c1cc7
odysseus5757 <stephen.cowen@gmail.com>
2015-06-03 17:15:42 -0700

    updated_further

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,9 @@ class Message < ActiveRecord::Base
   validates :classification_name, :presence => true, :uniqueness => true
 
   belongs_to :matter
-  belongs_to :user
-  belongs_to :classification
+  belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
+
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :username, :presence => true, :uniqueness => true
 
   has_many :matters
-  has_many :messages
+  has_many :messages, :foreign_key => 'sender_id', :primary_key => 'user_id'
 
 end
